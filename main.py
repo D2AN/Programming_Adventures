a1f08a50f98f629c72372a6a0e45007137bd43af
nikas <nsurgautas1@gmail.com>
2023-09-28 17:59:04 +0300

    ka6kas

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-from tkinter import *
-from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+# move an Image on the canvas with tkinter
+ 
+import tkinter as tk
+from tkinter import*
+from PIL import Image, ImageTk
+# Create the window with the Tk class
+root = tk.Tk()
+eplotis = root.winfo_screenwidth()
+eaukstis = root.winfo_screenheight()
+veikejoaukstis = round(eaukstis / 15)
+veikejoplotis = round(eplotis / 15)
+x = 1
+y = 1
+# Create the canvas and make it visible with pack()
+canvas = tk.Canvas(root, width=eplotis, height=eaukstis)
+canvas.pack() # this makes it visible
+
+# Loads and create image (put the image in the folder)
+image = Image.open("menas.png")
+image = image.resize((veikejoplotis,veikejoaukstis), Image.ANTIALIAS)
+pic = ImageTk.PhotoImage(image)
+image = canvas.create_image(1, 1, anchor=tk.NW, image=pic)
+ 
+ 
+def move(event):
+    """Move the sprite image with a d w and s when click them"""
+    if event.char == "a":
+        canvas.move(image, -10, 0)
+    elif event.char == "d":
+        canvas.move(image, 10, 0)
+    elif event.char == "w":
+        canvas.move(image, 0, -10)
+    elif event.char == "s":
+        canvas.move(image, 0, 10)
+ 
+# This bind window to keys so that move is called when you press a key
+root.bind("<Key>", move)
+ 
+# this creates the loop that makes the window stay 'active'
 root.mainloop()
