c58421cbb7a0c06213f23646c157d9b9f886ad44
nikas <nsurgautas1@gmail.com>
2023-09-29 22:18:19 +0300

    lol

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import os
 
 # Initialize Pygame
 pygame.init()
-
+screen_info = pygame.display.Info()
+eaukstis = screen_info.current_h
+eplotis = screen_info.current_w
 # Set up the display
-screen = pygame.display.set_mode((800, 500))
+screen = pygame.display.set_mode((eplotis, eaukstis))
 pygame.display.set_caption("Pradžia")
 
 # Colors
@@ -25,29 +27,30 @@ main_text_rect = main_text.get_rect()
 main_text_x = 100
 main_text_y = 50
 main_text_rect.topleft = (main_text_x, main_text_y)
-
+puse = eplotis / 2
+aukstis = eaukstis / 8
 # Button texts
-kordinate1 = [100, 100]
-kordinate2 = [100, 10]
-kordinate3 = [100, 200]
+kordinate1 = [puse, eaukstis - aukstis]
+kordinate2 = [puse, eaukstis - aukstis * 2]
+kordinate3 = [puse, eaukstis - aukstis * 3]
 button_font = pygame.font.Font(None, 24)
-button1_text = button_font.render("Mokykis", True, BLACK)
+button1_text = button_font.render("Learnig", True, BLACK)
 button1_rect = button1_text.get_rect()
 button1_x = 0
 button1_y = 50
 button1_rect.topleft = (kordinate1[0] + 30 , kordinate1[1] + 30 )
 
-button2_text = button_font.render("Testuokis", True, BLACK)
+button2_text = button_font.render("Tests", True, BLACK)
 button2_rect = button2_text.get_rect()
 button2_x = 0
 button2_y = 150
-button2_rect.topleft = (button2_x, button2_y)
+button2_rect.topleft = (kordinate2[0] + 30 , kordinate2[1] + 30 )
 buton = ['buton.png','butononmause.png']
-button3_text = button_font.render("Žaisk", True, BLACK)
+button3_text = button_font.render("Play", True, BLACK)
 button3_rect = button3_text.get_rect()
 button3_x = 0
 button3_y = 250
-button3_rect.topleft = (button3_x, button3_y)
+button3_rect.topleft = (kordinate3[0] + 30 , kordinate3[1] + 30 )
 
 # Main program loop
 running = True
@@ -74,9 +77,12 @@ while running:
     pirmas_rect = pirmas.get_rect()
     antras_rect = antras.get_rect()
     trecias_rect = trecias.get_rect()
-    pirmas_rect = pirmas_rect[0] + kordinate1[0]
-    pirmas_rect = pirmas_rect[1] + kordinate1[1]
-    pirmas_rect = pirmas_rect.get_rect()
+    pirmas_rect.x = kordinate1[0]
+    pirmas_rect.y = kordinate1[1]
+    antras_rect.x = kordinate2[0]
+    antras_rect.y = kordinate2[1]
+    trecias_rect.x = kordinate3[0]
+    trecias_rect.y = kordinate3[1]
     screen.blit(pirmas, kordinate1)
     screen.blit(antras, kordinate2)
     screen.blit(trecias, kordinate3)
@@ -96,7 +102,6 @@ while running:
        trecias = pygame.image.load(buton[1])
     else:
         trecias = pygame.image.load(buton[0])
-    print(pirmas_rect)
 
     # ce darau migtukius
     screen.blit(pirmas, kordinate1)
