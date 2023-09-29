a57b8c60d887902ea4b6c7d5be65af0dd5d054bd
nikas <nsurgautas1@gmail.com>
2023-09-29 20:15:12 +0300

    heheneveike

--- main.py
+++ main.py
@@ -27,19 +27,22 @@ main_text_y = 50
 main_text_rect.topleft = (main_text_x, main_text_y)
 
 # Button texts
+kordinate1 = [100, 100]
+kordinate2 = [100, 10]
+kordinate3 = [100, 200]
 button_font = pygame.font.Font(None, 24)
 button1_text = button_font.render("Mokykis", True, BLACK)
 button1_rect = button1_text.get_rect()
 button1_x = 0
 button1_y = 50
-button1_rect.topleft = (button1_x, button1_y)
+button1_rect.topleft = (kordinate1[0] + 30 , kordinate1[1] + 30 )
 
 button2_text = button_font.render("Testuokis", True, BLACK)
 button2_rect = button2_text.get_rect()
 button2_x = 0
 button2_y = 150
 button2_rect.topleft = (button2_x, button2_y)
-
+buton = ['buton.png','butononmause.png']
 button3_text = button_font.render("Žaisk", True, BLACK)
 button3_rect = button3_text.get_rect()
 button3_x = 0
@@ -58,32 +61,48 @@ while running:
                 x, y = event.pos
 
                 # Check which button the user pressed and run the corresponding file
-                if 0 <= x <= 100 and 0 <= y <= 100:
+                if pirmas_rect.collidepoint(pygame.mouse.get_pos()):
                     run_game("bandomasis.py")
-                        
-                elif 0 <= x <= 100 and 100 <= y <= 200:
+                if antras_rect.collidepoint(pygame.mouse.get_pos()):
                     run_game("paint.py")
-                        
-                elif 0 <= x <= 100 and 200 <= y <= 300:
+                if trecias_rect.collidepoint(pygame.mouse.get_pos()):
                     run_game("game.py")
-                    
-   
+    
+    pirmas = pygame.image.load(buton[0])
+    antras = pygame.image.load(buton[0])
+    trecias = pygame.image.load(buton[0])
+    pirmas_rect = pirmas.get_rect()
+    antras_rect = antras.get_rect()
+    trecias_rect = trecias.get_rect()
+    pirmas_rect = pirmas_rect[0] + kordinate1[0]
+    pirmas_rect = pirmas_rect[1] + kordinate1[1]
+    pirmas_rect = pirmas_rect.get_rect()
+    screen.blit(pirmas, kordinate1)
+    screen.blit(antras, kordinate2)
+    screen.blit(trecias, kordinate3)
     # Draw the screen
     screen.fill(WHITE)
 
-    # Draw the buttons
-    pirmas = pygame.draw.rect(screen, (0, 255, 0), (0, 0, 100, 100))
-    antras = pygame.draw.rect(screen, (0, 0, 255), (0, 100, 100, 100))
-    trecias = pygame.draw.rect(screen, (255, 0, 0), (0, 200, 100, 100))
-    if pirmas.collidepoint(pygame.mouse.get_pos()):
-        pygame.draw.rect(screen, (0, 200, 0), (0, 0, 100, 100))
-    if antras.collidepoint(pygame.mouse.get_pos()):
-        pygame.draw.rect(screen, (0, 0, 200), (0, 100, 100, 100))
-    if trecias.collidepoint(pygame.mouse.get_pos()):
-        pygame.draw.rect(screen, (200, 0, 0), (0, 200, 100, 100))
+    
+    if pirmas_rect.collidepoint(pygame.mouse.get_pos()):
+       pirmas = pygame.image.load(buton[1])
+    else:
+        pirmas = pygame.image.load(buton[0])
+    if antras_rect.collidepoint(pygame.mouse.get_pos()):
+       antras = pygame.image.load(buton[1])
+    else:
+        antras = pygame.image.load(buton[0])
+    if trecias_rect.collidepoint(pygame.mouse.get_pos()):
+       trecias = pygame.image.load(buton[1])
+    else:
+        trecias = pygame.image.load(buton[0])
+    print(pirmas_rect)
 
-
-    # Draw the texts
+    # ce darau migtukius
+    screen.blit(pirmas, kordinate1)
+    screen.blit(antras, kordinate2)
+    screen.blit(trecias, kordinate3)
+    # uzrasai
     screen.blit(main_text, main_text_rect)
     screen.blit(button1_text, button1_rect)
     screen.blit(button2_text, button2_rect)
---
+++ pelyte.py
@@ -0,0 +1,5 @@
+import pygame
+
+def nustatyti_pelės_išvaizdą():
+    pelės_išvaizda = pygame.image.load('pelyte.png')
+    return pelės_išvaizda
