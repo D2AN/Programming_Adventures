939ff1fe026c16750e4cdfd761621b9097f1782d
nikas <nsurgautas1@gmail.com>
2023-10-02 12:37:59 +0300

    comit

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ pygame.init()
 screen_info = pygame.display.Info()
 eaukstis = screen_info.current_h
 eplotis = screen_info.current_w
-# eplotis = 800
-# eaukstis = 400
+
 # Set up the display
 screen = pygame.display.set_mode((eplotis, eaukstis))
 pygame.display.set_caption("Pradžia")
@@ -17,14 +16,15 @@ BLACK = (0, 0, 0)
 # Function to run when a button is pressed
 def run_game(game_file):
     os.system(f"python {game_file}")
-
-font = pygame.font.Font(None, 36)  # specify font and size
+textdydis = 50
+font = pygame.font.Font(None, 0)  # specify font and size
 
 # Main text
 # ///////////////////////////////////////////////////////////
 puse = eplotis / 2
 main_text = font.render("Programing adventures", True, BLACK)
 main_text_rect = main_text.get_rect()
+main_text_rect.size = textdydis
 main_text_x = puse - puse / 2
 main_text_y = 50
 main_text_rect.topleft = (main_text_x, main_text_y)
@@ -35,12 +35,15 @@ aukstis = eplotis / 5
 kordinate1 = [puse - eaukstis / 6, eaukstis - aukstis]
 kordinate2 = [puse - eaukstis / 6, eaukstis - aukstis - aukstis / 2]
 kordinate3 = [puse - eaukstis / 6, eaukstis - aukstis - aukstis ]
+išeiti = [puse + puse / 8, eaukstis - eaukstis / 6 ]
 # ///////////////////////////////////////////////
 buton = ['buton.png','butononmause.png']
 bdydis = [(aukstis ,eaukstis / 8),(aukstis + 10,eaukstis / 8 +10)]
 pirmas = pygame.image.load(buton[0])
 antras = pygame.image.load(buton[0])
 trecias = pygame.image.load(buton[0])
+iseiti = pygame.image.load(buton[0])
+iseiti_rect = iseiti.get_rect()
 pirmas_rect = pirmas.get_rect()
 antras_rect = antras.get_rect()
 trecias_rect = trecias.get_rect()
@@ -50,7 +53,9 @@ antras_rect.x = kordinate2[0]
 antras_rect.y = kordinate2[1]
 trecias_rect.x = kordinate3[0]
 trecias_rect.y = kordinate3[1]
-trecias_rect.size = antras_rect.size = pirmas_rect.size = bdydis[0]
+iseiti_rect.x = išeiti[0]
+iseiti_rect.y = išeiti[1]
+iseiti_rect.size = trecias_rect.size = antras_rect.size = pirmas_rect.size = bdydis[0]
 # ///////////////////////////////////////////////
 button_font = pygame.font.Font(None, 40)
 button1_text = button_font.render("Learnig", True, BLACK)
@@ -90,6 +95,9 @@ while running:
                     run_game("paint.py")
                 if trecias_rect.collidepoint(pygame.mouse.get_pos()):
                     run_game("game.py")
+                if iseiti_rect.collidepoint(pygame.mouse.get_pos()):
+                    pygame.quit()
+                    sys.exit()
     # Draw the screen
    
     screen.blit(galas,(0,0))
@@ -111,6 +119,12 @@ while running:
     else:
         trecias = pygame.image.load(buton[0])
         screen.blit(pygame.transform.scale(trecias, bdydis[0]), kordinate3)
+    if iseiti_rect.collidepoint(pygame.mouse.get_pos()):
+       iseiti = pygame.image.load(buton[1])
+       screen.blit(pygame.transform.scale(iseiti, bdydis[1]), išeiti)
+    else:
+        iseiti = pygame.image.load(buton[0])
+        screen.blit(pygame.transform.scale(iseiti, bdydis[0]), išeiti)
 
     
     # uzrasai
