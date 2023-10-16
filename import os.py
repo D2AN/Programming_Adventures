70b48378a107f31da3e7c58825968c545b35927f
nikas <nsurgautas1@gmail.com>
2023-10-16 19:12:05 +0300

    pakeitimai :D

--- import os.py
+++ import os.py
@@ -1,5 +1,6 @@
 import os
-import subprocess
+import requests
+import zipfile
 import shutil
 
 # GitHub repozitorijos savininko vardas ir repozitorijos pavadinimas
@@ -12,31 +13,48 @@ appdata_folder = os.path.expanduser(f"~\\AppData\\Roaming\\PAadventures")
 # Sukurkite `AppData` katalogą, jei jis neegzistuoja
 os.makedirs(appdata_folder, exist_ok=True)
 
-# Git komanda norint atsisiųsti repozitoriją
-git_clone_command = f"git clone https://github.com/{owner}/{repo_name}.git {appdata_folder}"
+# Nuoroda į repozitorijos ZIP archyvą naudojant GitHub API
+github_api_url = f"https://api.github.com/repos/{owner}/{repo_name}/zipball/D2AN"
 
-# Atsisiųskite repozitoriją naudodami Git komandą
-subprocess.run(git_clone_command, shell=True)
+# Nurodykite ZIP failo pavadinimą
+zip_file_name = f"{owner}-{repo_name}-D2AN.zip"
 
-# Nurodykite exe failo pavadinimą
-exe_name = "PAdventures.lnk"
+# Atsisiųskite ZIP archyvą
+response = requests.get(github_api_url)
+if response.status_code == 200:
+    zip_file_path = os.path.join(appdata_folder, zip_file_name)
+    with open(zip_file_path, 'wb') as zip_file:
+        zip_file.write(response.content)
+    print(f"ZIP archyvas atsisiųstas: {zip_file_path}")
 
-# Nurodykite kelią į exe failą
-exe_path = os.path.join(appdata_folder, exe_name)
+    # Išskleiskite ZIP archyvą į katalogą
+    extract_folder = os.path.join(appdata_folder, f"{owner}-{repo_name}-D2AN")
+    os.makedirs(extract_folder, exist_ok=True)
 
-# Tikriname, ar exe failas yra
-if os.path.exists(exe_path):
-    print(f"{exe_name} failas suinstaliuotas.")
+    # Išskleiskite ZIP archyvą
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_folder)
 
-    # Katalogas, kuriame yra darbalaukio nuorodos
-    desktop_folder = os.path.expanduser(f"~\\Desktop")
+    # Nurodykite exe failo pavadinimą
+    exe_name = "PAdventures.lnk"
 
-    # Kelias į darbalaukio nuorodą
-    desktop_link_path = os.path.join(desktop_folder, exe_name)
+    # Nurodykite kelią į exe failą
+    exe_path = os.path.join(extract_folder, f"{owner}-{repo_name}-D2AN-main", exe_name)
 
-    # Perkelkite nuorodą į darbalaukį
-    shutil.move(exe_path, desktop_link_path)
+    if os.path.exists(exe_path):
+        print(f"{exe_name} failas suinstaliuotas.")
 
-    print(f"{exe_name} nuoroda perkelta į darbalaukį.")
+        # Katalogas, kuriame yra darbalaukio nuorodos
+        desktop_folder = os.path.expanduser(f"~\\Desktop")
+
+        # Kelias į darbalaukio nuorodą
+        desktop_link_path = os.path.join(desktop_folder, exe_name)
+
+        # Perkelkite nuorodą į darbalaukį
+        shutil.move(exe_path, desktop_link_path)
+
+        print(f"{exe_name} nuoroda perkelta į darbalaukį.")
+    else:
+        print(f"{exe_name} failas nerastas. Kelias: {exe_path}")
 else:
-    print(f"{exe_name} failas nerastas. Kelias: {exe_path}")
+    print(f"Nepavyko atsisiųsti ZIP archyvo. HTTP statusas: {response.status_code}")
